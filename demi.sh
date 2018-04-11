ea97c9872850b0df82b18a2d8113dccd048de0ea
adalenv <adaleenvladi@gmail.com>
2018-04-11 05:33:54 -0400

    Typo Fix

--- demi.sh
+++ demi.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-clear
 killall -9 watch
-gnome-terminal --command="watch -n 2 'cat bambam.txt'"
+gnome-terminal --command="watch -n 1 'tail -n 1 bambam.txt'"
+clear
 nmap_path=`which nmap`
 file_name=`echo $1 | awk -F "/" '{print $1}'`
-echo 'running nmap'
+echo 'Running nmap'
 $nmap_path -T5 -sT -p 80 --open $1 -oG - | awk '$4=="Ports:"{print $2,":"$5}' | awk -F "/" '{print $1}' > output_`echo $1 | awk -F "/" '{print $1}'`.txt
 
  #file_content=`cat output_$file_name.txt`
  IFS=$'\r\n' GLOBIGNORE='*' command eval  'arr=($(cat output_$file_name.txt))'
 
-echo 'nmap finished'
+echo 'Nmap finished'
 for i in "${arr[@]}"
 do
 
 	#sh ./mt.sh  echo -n "${arr[3]//[[:space:]]/}"
-	echo  'testing : '$i
+	echo  'Testing : '$i
 	ver=`curl --connect-timeout 3 -m 3 -s $i | grep -i RouterOS | awk -F ">" {'print $2'} | awk -F "<" {'print $1'} | head -2 | grep -v "configura" | awk -F "v" {'print $2'} | awk -F "." {'print $2"."$3'}` 
 	ver_total=`curl --connect-timeout 3 -m 3 -s $i | grep -i RouterOS | awk -F ">" {'print $2'} | awk -F "<" {'print $1'} | head -2 | grep -v "configura" | awk -F "v" {'print $2'} `
 	version_ok=38.5
@@ -28,9 +28,9 @@ if [ -z "$ver" ]
 			ip=`echo $i | awk -F ":" '{print $1}'`
 			port=`echo $i | awk -F ":" '{print $2}'`
 			echo $ip $port $ver_total
-			echo 'testing with mipsbe binary...'
+			echo 'Testing with mipsbe binary...'
 			./mt.sh $ip $ver_total  $port 'mipsbe'
-			echo 'testing with x86 binary...'
+			echo 'Testing with x86 binary...'
 			./mt.sh $ip $ver_total  $port 'x86'
 		else
 			echo "Not supported" #> /dev/null
